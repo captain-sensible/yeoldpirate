91c1a14e5b49d116f9f53f8992dd121ae23d5d0f
andy brookes <andybrookestar@gmail.com>
2017-06-14 16:57:06 +0000

    addressed WP issues

--- Ye-old-pirate.php
+++ Ye-old-pirate.php
@@ -1,13 +1,13 @@
 <?php
 /*
 Plugin Name: Ye Old Pirate
-Plugin URI: https://github.com/captain-sensible/yeoldpirate
+Plugin URI: https://github.com/captain-sensible/yeoldpirate/releases
 Description: tweaks text in existing posts  & adds pirate speak 
 to them,this is a very basic plugin & uses basic concepts seen in the Hello Dolly 
 plugin & easy to understand native PHP
 Author: Andy Brookes
 Author URI:https://github.com/captain-sensible
-Version: 1.0
+Version: 1.02
 Licence:GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -16,67 +16,67 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 
 
-function replace($text)
+function yop_pirateTweakText($text)
 {
 $tofilter = $text;
 
 
-$andy = array(
-		"the"=>			"Ye olde   ",
-		"yes"=>			"aye ",
-		"stagnant"=> 	"addle",
-		"hey"=>			 "ahoy",
-		"call"=>		"hail" ,
-		"crew"=>		"hands",
-		"clothes"=>		"togs",
-		"work shy"=>	 "Abbey Llubber",
-		"beggers"=>		"Abraham Men ",
-		"afraid"=>		"AFEARD",
-		"hang"=>		"Short Drop and a  quick stop",
-		"stop"=>		"Avast",
-		"cover"=>		"Batten Down", 
-		"minion"=>		"Before the mast",
-		"goods"=>		"booty",
+$yopVar = array(
+		"the"=>				"Ye olde",
+		"yes"=>				"aye ",
+		"stagnant"=> 		"addle",
+		"hey"=>			 	"ahoy",
+		"call"=>			"hail" ,
+		"crew"=>			"hands",
+		"clothes"=>			"togs",
+		"work shy"=>	 	"Abbey Lubber",
+		"beggers"=>			"Abraham Men ",
+		"afraid"=>			"Afeard",
+		"hang"=>			"Short Drop and a  quick stop",
+		"stop"=>			"Avast",
+		"cover"=>			"Batten Down", 
+		"minion"=>			"Before the mast",
+		"goods"=>			"booty",
 		"work shy"=>		"Abbey Lubber",
-		"dawn"=>		"Aurora ",
-		"duelling pistols"	=>"barking irons",
-		"eggs"=>		"cackle fruit",
-		"barrel"=>		"cask",
-		"knife"=>		"chivey",
-		"stupid"=>		 " Chowder Headed",
-		"hanged "=> 	" Dance The Hempen Jig",
-		"rum"=> 		"grog ",
-		" drunk"=>		 "Loaded To The GunnellS ",
-		"mop "=> 		" swab",
-		"pirate "=> 	"Buccaneer ",
-		"push"=> 		" heave ho ",
-		"sabotage"=> 	"Scuttle  ",
-		"clean "=> 		" ship shape",
-		" you"=> 		" ye",
-		"pay attention "=> " avast ye",
+		"dawn"=>			"Aurora ",
+		"duelling pistols"=>"barking irons",
+		"eggs"=>			"cackle fruit",
+		"barrel"=>			"cask",
+		"knife"=>			"chivey",
+		"stupid"=>		 	" Chowder Headed",
+		"hanged "=> 		" Dance The Hempen Jig",
+		"rum"=> 			"grog ",
+		" drunk"=>		 	"Loaded To The GunnellS ",
+		"mop "=> 			" swab",
+		"pirate "=> 		"Buccaneer ",
+		"push"=> 			" heave ho ",
+		"sabotage"=> 		"Scuttle  ",
+		"clean "=> 			" ship shape",
+		" you"=> 			" ye",
+		"pay attention "=> 	" avast ye",
 		" cheat"=> 			"Hornswaggle ",
 		"is"=>				"there be",
-		"are"=>			"bist",
-		"fool"=>		"Bobolyne",
-		"whinger"=>		"Gnashgab",
-		"lazy"=>		"Loitter-Sack",
-		"smelly"=>		"Stymphalist",
-		"basement"=>	"bilge",
-		"cockroach"=>	"bombay runner",
-		"pull"=>		"bowse",
-		"wave"=>		"breaker",
-		"leave"=>		"disembark",
-		"rubbish"=>		"gash",
-		"sailor"=>		"jack",
-		"tie"=>			"lash",
-		"no"=>			"nay",
+		"are"=>				"bist",
+		"fool"=>			"Bobolyne",
+		"whinger"=>			"Gnashgab",
+		"lazy"=>			"Loitter-Sack",
+		"smelly"=>			"Stymphalist",
+		"basement"=>		"bilge",
+		"cockroach"=>		"bombay runner",
+		"pull"=>			"bowse",
+		"wave"=>			"breaker",
+		"leave"=>			"disembark",
+		"rubbish"=>			"gash",
+		"sailor"=>			"jack",
+		"tie"=>				"lash",
+		"no"=>				"nay",
 		"\.."=>           "  me hearties .",
 			
 		);
 
 
 
-foreach ($andy as $key => $value) {
+foreach ($yopVar as $key => $value) {
  
 
 
@@ -90,7 +90,8 @@ return $tofilter;
 
 }
 
-add_filter( 'the_content', 'replace' );
+
+add_filter( 'the_content', 'yop_pirateTweakText' );
 
 
 
